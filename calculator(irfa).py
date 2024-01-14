6c02ac526cb5ae0187ce10776ecdaea3b3e85923
Irfaiqbal <156583532+Irfaiqbal@users.noreply.github.com>
2024-01-14 17:33:47 +0500

    Add files via upload

---
+++ calculator(irfa).py
@@ -0,0 +1,70 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:\Users\Iruj Iqbal\OneDrive\Downloads\input.py (irfa).py ===========
+
+1. add
+2. subtract
+3. multiply
+4. divide
+
+enter your number value num123
+enter your number value num256
+Select the option 1. add
+2. subtract
+3. multiply
+4. divide
+
+enter your opr...1
+79
+
+============ RESTART: C:\Users\Iruj Iqbal\OneDrive\Downloads\input.py (irfa).py ===========
+
+1. add
+2. subtract
+3. multiply
+4. divide
+
+enter your number value num125
+enter your number value num256
+Select the option 1. add
+2. subtract
+3. multiply
+4. divide
+
+enter your opr...2
+-31
+
+============ RESTART: C:\Users\Iruj Iqbal\OneDrive\Downloads\input.py (irfa).py ===========
+
+1. add
+2. subtract
+3. multiply
+4. divide
+
+enter your number value num156
+enter your number value num245
+Select the option 1. add
+2. subtract
+3. multiply
+4. divide
+
+enter your opr...3
+2520
+>>> 
+============ RESTART: C:\Users\Iruj Iqbal\OneDrive\Downloads\input.py (irfa).py ===========
+
+1. add
+2. subtract
+3. multiply
+4. divide
+
+enter your number value num125
+enter your number value num255
+Select the option 1. add
+2. subtract
+3. multiply
+4. divide
+
+enter your opr...4
+0.45454545454545453
